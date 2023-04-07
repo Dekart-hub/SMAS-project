272b61d879883c9f3bea99cff0c79e182d8c109e
Max <dmitmax2002@yandex.ru>
2023-04-07 19:03:04 +0300

    ~ отдельные атрибуты моделей

--- web/models.py
+++ web/models.py
@@ -2,58 +2,74 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+TEXT_LENGTH = 500
+TITLE_LENGTH = 25
+TAG_LENGTH = 8
+PRICE_LENGTH = 14
+DECIMAL_PRICE_LENGTH = 4
+
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Пользователь')
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True, verbose_name='Аватарка')
 
     class Meta:
-        verbose_name = 'пользователь'
-        verbose_name_plural = 'пользователи'
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
 
 
 class StockMarket(models.Model):
-    title = models.CharField(max_length=50, verbose_name='Название')
-    tag = models.CharField(max_length=8, verbose_name='Тэг')
-    description = models.CharField(max_length=500, default='-', verbose_name='Описание')
+    title = models.CharField(max_length=TITLE_LENGTH, verbose_name='Название')
+    tag = models.CharField(max_length=TAG_LENGTH, verbose_name='Тэг')
+    description = models.CharField(max_length=TEXT_LENGTH, default=None, verbose_name='Описание')
 
     class Meta:
-        verbose_name = 'биржа'
-        verbose_name_plural = 'биржи'
+        verbose_name = 'Биржа'
+        verbose_name_plural = 'Биржи'
 
 
 class StockType(models.Model):
-    title = models.CharField(max_length=15, verbose_name='Название')
-    description = models.CharField(max_length=500, default='-', verbose_name='Описание')
+    title = models.CharField(max_length=TITLE_LENGTH, verbose_name='Название')
+    description = models.CharField(max_length=TEXT_LENGTH, default=None, verbose_name='Описание')
 
     class Meta:
-        verbose_name = 'тип ценной бумаги'
-        verbose_name_plural = 'типы ценных бумаг'
+        verbose_name = 'Тип ценной бумаги'
+        verbose_name_plural = 'Типы ценных бумаг'
 
 
 class StockInformation(models.Model):
-    title = models.CharField(max_length=20, verbose_name='Название')
-    tag = models.CharField(max_length=8, verbose_name='Тэг')
-    description = models.CharField(max_length=500, default='-', verbose_name='Описание')
+    title = models.CharField(max_length=TITLE_LENGTH, verbose_name='Название')
+    tag = models.CharField(max_length=TAG_LENGTH, verbose_name='Тэг')
+    description = models.CharField(max_length=TEXT_LENGTH, default=None, verbose_name='Описание')
     market = models.ForeignKey(StockMarket, on_delete=models.CASCADE, verbose_name='Биржа')
     type = models.ForeignKey(StockType, on_delete=models.CASCADE, verbose_name='Тип бумаги')
 
     class Meta:
-        verbose_name = 'сводка по бумаге'
-        verbose_name_plural = 'сводки по бумагам'
+        verbose_name = 'Сводка по бумаге'
+        verbose_name_plural = 'Сводки по бумагам'
 
 
 class Stock(models.Model):
     date = models.DateTimeField(verbose_name='Дата')
-    open = models.DecimalField(max_digits=14, decimal_places=4, verbose_name='Цена открытия')
-    high = models.DecimalField(max_digits=14, decimal_places=4, verbose_name='Максимальная цена')
-    low = models.DecimalField(max_digits=14, decimal_places=4, verbose_name='Минимальная цена')
-    close = models.DecimalField(max_digits=14, decimal_places=4, verbose_name='Цена закрытия')
+    open = models.DecimalField(max_digits=PRICE_LENGTH,
+                               decimal_places=DECIMAL_PRICE_LENGTH,
+                               verbose_name='Цена открытия')
+    high = models.DecimalField(max_digits=PRICE_LENGTH,
+                               decimal_places=DECIMAL_PRICE_LENGTH,
+                               verbose_name='Максимальная цена')
+    low = models.DecimalField(max_digits=PRICE_LENGTH,
+                              decimal_places=DECIMAL_PRICE_LENGTH,
+                              verbose_name='Минимальная цена')
+    close = models.DecimalField(max_digits=PRICE_LENGTH,
+                                decimal_places=DECIMAL_PRICE_LENGTH,
+                                verbose_name='Цена закрытия', default=None, null=True)
     volume = models.IntegerField(verbose_name='Объём продаж')
-    stock = models.ForeignKey(StockInformation, on_delete=models.CASCADE, verbose_name='Ценная бумага')
+    stock = models.ForeignKey(StockInformation, on_delete=models.CASCADE,
+                              verbose_name='Ценная бумага')
 
     class Meta:
-        verbose_name = 'ценная бумага'
-        verbose_name_plural = 'ценные бумаги'
+        verbose_name = 'Ценная бумага'
+        verbose_name_plural = 'Ценные бумаги'
 
 
 class CustomTemplate(models.Model):
@@ -61,5 +77,5 @@ class CustomTemplate(models.Model):
     template = models.JSONField(verbose_name='Шаблон в формате JSON')
 
     class Meta:
-        verbose_name = 'шаблон'
-        verbose_name_plural = 'шаблоны'
+        verbose_name = 'Шаблон'
+        verbose_name_plural = 'Шаблоны'
