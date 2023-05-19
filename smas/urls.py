42e07e4166c7010267d6be87a12e705f945209d7
Max <dmitmax2002@yandex.ru>
2023-05-19 17:53:52 +0300

    + пропуск строк

--- smas/urls.py
+++ smas/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 
+
 urlpatterns = [
     path("", include("web.urls")),
     path('admin/', admin.site.urls),
