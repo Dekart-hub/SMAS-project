20968e7b2710a0cabbacc408558cb7e6c799772d
Max <dmitmax2002@yandex.ru>
2023-05-21 22:06:22 +0300

    ~ папка сохранения моделей

--- web/services.py
+++ web/services.py
@@ -24,7 +24,7 @@ def take_date(date):
 
 
 def linear_regression_predict(x_test, name):
-    pkl_filename = f"lr_model_{name}.pkl"
+    pkl_filename = MEDIA_ROOT + "models/" + f"lr_model_{name}.pkl"
     with open(pkl_filename, 'rb') as file:
         pickle_model = pickle.load(file)
     return pickle_model.predict(x_test.reshape(-1, 1))
@@ -33,7 +33,7 @@ def linear_regression_predict(x_test, name):
 def linear_regression_train(x_train, y_train, name):
     lr = linear_model.LinearRegression()
     lr.fit(x_train.reshape(-1, 1), y_train.reshape(-1, 1))
-    pkl_filename = MEDIA_ROOT + "/models/" + f"lr_model_{name}.pkl"
+    pkl_filename = MEDIA_ROOT + "models/" + f"lr_model_{name}.pkl"
     with open(pkl_filename, 'wb') as file:
         pickle.dump(lr, file)
 
